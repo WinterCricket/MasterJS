847eb48c4834aad71ea2f883fac5eed81d0519bc
wintercricket <hodhodhod2003@gmail.com>
2021-10-09 18:00:47 -0500

    Add two files html and js

---
+++ fundamentally-part1/script.js
@@ -0,0 +1,9 @@
+let js = "javascript, okay?";
+
+console.log(40 * 8 + 23 - 40);
+
+let country = "Spain";
+let continent = "Europe";
+let population = 28;
+
+console.log(country, continent + " population is " + population);
