184b68116c93dd71481a8bf63541b229e9a0a89f
wintercricket <hodhodhod2003@gmail.com>
2021-10-18 19:25:47 -0500

    Create nested functions to find boolean

--- fundamentally-part1/script.js
+++ fundamentally-part1/script.js
@@ -1,12 +1,35 @@
-const isIsland = false;
+// const isIsland = false;
 
-const population = 64000000;
+// const population = 64000000;
 
-const country = 'Canada';
+// const country = 'Canada';
 
-const language = 'French';
+// const language = 'French';
 
-console.log(typeof isIsland, typeof population);
-// console.log(typeof );
-console.log(typeof country);
-console.log(typeof language);
+// console.log(typeof isIsland, typeof population);
+// // console.log(typeof );
+// console.log(typeof country);
+// console.log(typeof language);
+
+const massMark = 71;
+const heightMark = 1.8;
+
+const massJohn = 78;
+const heightJohn = 1.7;
+
+function functionBMI(mass, height) {
+	return (BMI = mass / height ** 2);
+}
+
+function markHigherBMI() {
+	let markBMI = functionBMI(massMark, heightMark);
+	let johnBMI = functionBMI(massJohn, heightJohn);
+
+	if (markBMI > johnBMI) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+console.log(markHigherBMI());
