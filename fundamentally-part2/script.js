0d760ef51e0f953bad1548c006e6385b551933f0
wintercricket <hodhodhod2003@gmail.com>
2021-10-26 20:59:38 -0500

    Use function declaration

--- fundamentally-part2/script.js
+++ fundamentally-part2/script.js
@@ -2,15 +2,24 @@
 
 const domID = document.getElementById('domID');
 
-function twoPies(first, second) {
-	const pies = `Make two pies: ${first} pie and ${second} pie.`;
-	return pies;
+// function twoPies(first, second) {
+// 	const pies = `Make two pies: ${first} pie and ${second} pie.`;
+// 	return pies;
+// }
+
+// const mummyPies = twoPies('licorice', 'ginger');
+
+// const kidPies = twoPies('M&M', 'Ice Cream');
+
+// const adultPies = twoPies('whisky-peach', 'sherry-shrimp');
+
+// domID.innerHTML = kidPies;
+
+//function declaration
+
+function calcAgeism1(birthYear) {
+	return 2037 - birthYear;
 }
-
-const mummyPies = twoPies('licorice', 'ginger');
-
-const kidPies = twoPies('M&M', 'Ice Cream');
-
-const adultPies = twoPies('whisky-peach', 'sherry-shrimp');
-
-domID.innerHTML = kidPies;
+const ageism1 = calcAgeism1(1966);
+domID.innerHTML = `You will be ${ageism1} when the tech brings immortality.`;
+console.log(ageism1);
