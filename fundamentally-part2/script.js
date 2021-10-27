93a72748af1b0493c486b6681b0d0456236f6de2
wintercricket <hodhodhod2003@gmail.com>
2021-10-26 19:34:30 -0500

    Create type of pies variable used with function

--- fundamentally-part2/script.js
+++ fundamentally-part2/script.js
@@ -6,4 +6,5 @@ function twoPies(first, second) {
 	domID.innerHTML = `Make two pies: ${first} pie and ${second} pie.`;
 }
 
-twoPies('ice cream', 'pineapple');
+//const kidPies = twoPies('ice cream', 'pineapple');
+const adultPies = twoPies('shrimp', 'whiskey-peach');
