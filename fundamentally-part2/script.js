3fb0014698c48bfd95b5faaabc68f501ff7676f8
wintercricket <hodhodhod2003@gmail.com>
2021-10-27 22:10:02 -0500

    Arrow function with more

--- fundamentally-part2/script.js
+++ fundamentally-part2/script.js
@@ -25,13 +25,25 @@ const domID = document.getElementById('domID');
 // console.log(ageism1);
 
 //function expression using anonymous function
-const calcAgeism2 = function(birthYear) {
-	return 2037 - birthYear;
-};
-domID.innerHTML = `You will be made immortal in this function expression in your ${calcAgeism2(1966)}st year.`;
-console.log(calcAgeism2(1966));
+// const calcAgeism2 = function(birthYear) {
+// 	return 2037 - birthYear;
+// };
+// domID.innerHTML = `You will be made immortal in this function expression in your ${calcAgeism2(1966)}st year.`;
+// console.log(calcAgeism2(1966));
 
 //arrow function
-const calcAge3 = (birthYear) => 2035 - birthYear;
-const age3 = calcAge3(1966);
-domID.innerHTML = `They roused everone who was ${age3} years alive.`;
+// function calcAge3(birthYear) {
+// 	return 2035 - birthYear;
+// }
+
+// const age3 = calcAge3(1966);
+// domID.innerHTML = `They roused everone who was ${age3} years alive.`;
+
+const yearsToRetire = (birthYear) => {
+	const age = 2022 - birthYear;
+	const retirement = 67 - age;
+	return retirement;
+};
+
+const notWorking = yearsToRetire(1966);
+domID.innerHTML = `You're running out of runway, love. Just ${notWorking} more years and you'll end up in the trees without a soft landing.`;
