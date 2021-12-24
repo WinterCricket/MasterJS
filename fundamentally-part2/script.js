734c44d96a31ac5ed1d2cdc00ff234ae9d3369af
wintercricket <hodhodhod2003@gmail.com>
2021-12-24 01:33:24 -0600

    add elipsis to beginning of each day prediction

--- fundamentally-part2/script.js
+++ fundamentally-part2/script.js
@@ -147,7 +147,7 @@ const data2 = [12, 5, -5, 0, 4];
 const printForecast = function (arr) {
   let string = "";
   for (let i = 0; i < arr.length; i++) {
-    string = string + arr[i] + `°C in ${i + 1} days ... `;
+    string = string + `... ${arr[i]} °C in ${i + 1} days ... `;
   }
   domID.innerHTML = string;
 };
