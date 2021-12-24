8080ee84991cc1e8b4c9a729d4a747f1fb2f513f
wintercricket <hodhodhod2003@gmail.com>
2021-12-24 01:38:23 -0600

    correct way to add elip and project new string to html

--- fundamentally-part2/script.js
+++ fundamentally-part2/script.js
@@ -147,8 +147,9 @@ const data2 = [12, 5, -5, 0, 4];
 const printForecast = function (arr) {
   let string = "";
   for (let i = 0; i < arr.length; i++) {
-    string = string + `... ${arr[i]} °C in ${i + 1} days ... `;
+    string = string + `${arr[i]} °C in ${i + 1} days ... `;
   }
-  domID.innerHTML = string;
+  //domID.innerHTML = string;
+  domID.innerHTML = `...` + string;
 };
 printForecast(data2);
