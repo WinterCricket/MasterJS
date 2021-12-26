c5036cd4e282fc652db23450f2fbfa1383ceee60
wintercricket <hodhodhod2003@gmail.com>
2021-12-25 22:51:11 -0600

    use if statement for message element

--- what number/script.js
+++ what number/script.js
@@ -6,6 +6,8 @@
 // console.log(document.querySelector(".guess").value);
 
 document.querySelector(".check").addEventListener("click", function () {
-  console.log(document.querySelector(".guess").value);
-  document.querySelector(".message").innerHTML = "Number Thumper!";
+  const guess = Number(document.querySelector(".guess").value);
+  if (!guess) {
+    document.querySelector(".message").innerHTML = "⛔No Number!⛔";
+  }
 });
