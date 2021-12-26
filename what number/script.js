c684ea81d14a3566194d681ab2786e15edf74b98
wintercricket <hodhodhod2003@gmail.com>
2021-12-25 22:00:34 -0600

    use eventlistener

--- what number/script.js
+++ what number/script.js
@@ -1,6 +1,11 @@
 "use strict";
 
-document.querySelector(".message").innerHTML = "Number Thumper!";
-console.log(document.querySelector(".message").innerHTML);
-console.log((document.querySelector(".guess").value = 16));
-console.log(document.querySelector(".guess").value);
+// document.querySelector(".message").innerHTML = "Number Thumper!";
+// console.log(document.querySelector(".message").innerHTML);
+// console.log((document.querySelector(".guess").value = 16));
+// console.log(document.querySelector(".guess").value);
+
+document.querySelector(".check").addEventListener("click", function () {
+  console.log(document.querySelector(".guess").value);
+  document.querySelector(".message").innerHTML = "Number Thumper!";
+});
