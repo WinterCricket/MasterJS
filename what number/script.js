c14f4dfc318bf7c93767e1f2af24293b11aa9f22
wintercricket <hodhodhod2003@gmail.com>
2021-12-28 18:50:04 -0600

    reset each element needed to initial condition

--- what number/script.js
+++ what number/script.js
@@ -5,10 +5,10 @@
 // console.log((document.querySelector(".guess").value = 16));
 // console.log(document.querySelector(".guess").value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 const classMess = document.querySelector(".message");
-document.querySelector(".number").textContent = secretNumber;
+//document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -46,5 +46,12 @@ document.querySelector(".check").addEventListener("click", function () {
   }
 });
 document.querySelector(".again").addEventListener("click", function () {
-  document.location.reload(false);
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".guess").value = "";
 });
