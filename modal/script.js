77be76f8763d60f0ae12c8901cbc28bd3c3d1cbe
wintercricket <hodhodhod2003@gmail.com>
2021-12-30 00:55:04 -0600

    further refactoring

--- modal/script.js
+++ modal/script.js
@@ -5,18 +5,17 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsShowModal = document.querySelectorAll(".show-modal");
 
-for (let i = 0; i < btnsShowModal.length; i++)
-  btnsShowModal[i].addEventListener("click", function () {
-    console.log("Show Modal Button Clicked!");
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
-
-const closedModalFunction = function () {
+const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
+const showModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
-btnCloseModal.addEventListener("click", closedModalFunction);
-
-overlay.addEventListener("click", closedModalFunction);
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener("click", showModal);
+  btnCloseModal.addEventListener("click", closeModal);
+  overlay.addEventListener("click", closeModal);
+}
