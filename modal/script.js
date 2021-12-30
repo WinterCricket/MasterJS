6576fff2d1ad4e90ef019c5140a96eeaa1d0e00c
wintercricket <hodhodhod2003@gmail.com>
2021-12-30 00:17:26 -0600

    add for loop for adding event listener to modal states hidden unhidden

--- modal/script.js
+++ modal/script.js
@@ -3,4 +3,15 @@
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
-const btnsShowModal = document.querySelector(".show-modal");
+const btnsShowModal = document.querySelectorAll(".show-modal");
+
+for (let i = 0; i < btnsShowModal.length; i++)
+  btnsShowModal[i].addEventListener("click", function () {
+    console.log("Show Modal Button Clicked!");
+    modal.classList.remove("hidden");
+    //overlay.classList.remove("hidden");
+  });
+
+btnCloseModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+});
