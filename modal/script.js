10edc6f8e3fde1eab087f619503a61705044b554
wintercricket <hodhodhod2003@gmail.com>
2021-12-30 00:38:29 -0600

    refactor to dry function to perform methods

--- modal/script.js
+++ modal/script.js
@@ -12,12 +12,11 @@ for (let i = 0; i < btnsShowModal.length; i++)
     overlay.classList.remove("hidden");
   });
 
-btnCloseModal.addEventListener("click", function () {
+const closedModalFunction = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
-});
+};
 
-overlay.addEventListener("click", function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
+btnCloseModal.addEventListener("click", closedModalFunction);
+
+overlay.addEventListener("click", closedModalFunction);
