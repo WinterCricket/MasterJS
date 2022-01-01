1a98359542c26ec186e59a61ffd9cec64fb35c11
wintercricket <hodhodhod2003@gmail.com>
2022-01-01 03:19:40 -0600

    put methods into variables

--- dice-game/script.js
+++ dice-game/script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const score0El = document.querySelector("#score--0");
+const score1El = document.querySelector("#score--1");
+const score2El = document.querySelector("#score--2");
+const score3El = document.querySelector("#score--3");
+const score4El = document.querySelector("#score--4");
+const score5El = document.querySelector("#score--5");
+const score6El = document.querySelector("#score--6");
