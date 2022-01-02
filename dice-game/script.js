38b776022c999232a6ef7fa3e43fd2dd533f49a3
wintercricket <hodhodhod2003@gmail.com>
2022-01-01 20:38:16 -0600

    add tab icon and change icons

--- dice-game/script.js
+++ dice-game/script.js
@@ -7,3 +7,10 @@ const score3El = document.querySelector("#score--3");
 const score4El = document.querySelector("#score--4");
 const score5El = document.querySelector("#score--5");
 const score6El = document.querySelector("#score--6");
+
+const dice1 = document.querySelector(".dice");
+
+dice1.classList.add("hidden");
+
+score0E0.textContent = 0;
+score0E1.textContent = 0;
