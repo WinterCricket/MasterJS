b7bf4caffd5011f053eeff175e6419d25ae4dd41
wintercricket <hodhodhod2003@gmail.com>
2022-02-22 13:30:46 -0600

    experiment with variable scope in call stack

--- JSbehind-scene/script.js
+++ JSbehind-scene/script.js
@@ -1,15 +1,26 @@
 "use strict";
 
-const dom = document.querySelector(".execute");
-const som = document.querySelector(".show");
+// const dom = document.querySelector(".execute");
+// const som = document.querySelector(".show");
+// function calcAge(birthYear) {
+//   const age = 2335 - birthYear;
+//   function printAge() {
+//     const output = `${firstName}, you are ${age}, as you were born in ${birthYear}`;
+//     som.innerHTML = output;
+//   }
+//   printAge();
+//   return age + "Howdy, doofus!";
+// }
+// const firstName = "Geordie";
+// calcAge(1966);
+
 function calcAge(birthYear) {
-  const age = 2335 - birthYear;
-  function printAge() {
-    const output = `${firstName}, you are ${age}, as you were born in ${birthYear}`;
-    som.innerHTML = output;
+  const age = 2022 - birthYear;
+
+  if (age > 18) {
+    return console.log(`Welcome to my room!`);
+  } else {
+    return console.log(`Kids aren't allowed. You're only ${age}!`);
   }
-  printAge();
-  return age + "Howdy, doofus!";
 }
-const firstName = "Geordie";
-calcAge(1966);
+calcAge(2008);
