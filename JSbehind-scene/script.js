d5d448e5970f9318e07de01c31b3a816c5c6eb9a
wintercricket <hodhodhod2003@gmail.com>
2022-02-22 16:04:06 -0600

    set up app for call stack scope research

--- JSbehind-scene/script.js
+++ JSbehind-scene/script.js
@@ -1,26 +1,24 @@
 "use strict";
 
-// const dom = document.querySelector(".execute");
-// const som = document.querySelector(".show");
-// function calcAge(birthYear) {
-//   const age = 2335 - birthYear;
-//   function printAge() {
-//     const output = `${firstName}, you are ${age}, as you were born in ${birthYear}`;
-//     som.innerHTML = output;
-//   }
-//   printAge();
-//   return age + "Howdy, doofus!";
-// }
-// const firstName = "Geordie";
-// calcAge(1966);
+const dom = document.querySelector(".execute");
+const som = document.querySelector(".show");
 
 function calcAge(birthYear) {
   const age = 2022 - birthYear;
 
-  if (age > 18) {
-    return console.log(`Welcome to my room!`);
-  } else {
-    return console.log(`Kids aren't allowed. You're only ${age}!`);
+  function printAge() {
+    const output = `${firstName}, you are ${age}, as you were born in ${birthYear}`;
+    som.innerHTML = output;
+
+    if (birthYear >= 1965 && birthYear <= 1980) {
+      const genX = `${firstName}, you are a gen xer. To the front of the line! Congrats, you youthful ${age}!`;
+      dom.innerHTML = genX;
+    }
   }
+
+  printAge();
+  return age;
 }
-calcAge(2008);
+
+const firstName = "Geordie";
+calcAge(1976);
