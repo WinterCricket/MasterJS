0e29a2fac551e7d3fcfcc675e3a66ba5d8a55add
wintercricket <hodhodhod2003@gmail.com>
2022-02-28 00:42:54 -0600

    my random match app is done

--- MERN/percent-gen/percent.js
+++ MERN/percent-gen/percent.js
@@ -3,8 +3,9 @@ let rnd = Math.random();
 rnd = rnd * 100;
 rnd = Math.floor(rnd) + 1;
 
-// Math.floor(1 - 100);
-// const n = rnd + 1;
-console.log(rnd);
+// console.log(rnd);
 
-percentage.innerHTML = rnd;
+// percentage.innerHTML = rnd;
+
+let username = prompt("What are your names?");
+alert(`${username}, you have a ${rnd}% chance of being a match.`);
