3cc756549080a19a1ea847734e34959e398b40e1
wintercricket <hodhodhod2003@gmail.com>
2022-02-28 00:31:06 -0600

    use let variable develop mini app

--- MERN/percent-gen/percent.js
+++ MERN/percent-gen/percent.js
@@ -1,3 +1,10 @@
-let rnd = Math.random().Math.floor(1 - 100);
-const n = rnd + 1;
-console.log(n);
+const percentage = document.querySelector(".output");
+let rnd = Math.random();
+rnd = rnd * 100;
+rnd = Math.floor(rnd) + 1;
+
+// Math.floor(1 - 100);
+// const n = rnd + 1;
+console.log(rnd);
+
+percentage.innerHTML = rnd;
