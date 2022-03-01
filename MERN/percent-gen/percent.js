87413cdc99e14b31a445ac26f48a165f8ff198b5
wintercricket <hodhodhod2003@gmail.com>
2022-02-28 23:10:29 -0600

    my bmi calculator as a function

--- MERN/percent-gen/percent.js
+++ MERN/percent-gen/percent.js
@@ -1,29 +1,43 @@
 const percentage = document.querySelector(".output");
-let rnd = Math.random();
-rnd = rnd * 100;
-rnd = Math.floor(rnd) + 1;
+// let rnd = Math.random();
+// rnd = rnd * 100;
+// rnd = Math.floor(rnd) + 1;
 
-// console.log(rnd);
+// // console.log(rnd);
 
-// percentage.innerHTML = rnd;
+// // percentage.innerHTML = rnd;
 
-let username = prompt("What is your name?");
-prompt("What is the other's name?");
+// let username = prompt("What is your name?");
+// prompt("What is the other's name?");
 
-if (rnd < 70 && rnd > 50) {
-  alert(
-    `${username}, you have a ${rnd}% chance of being a match, which is okay. Go for it, but don't expect it to last!`
-  );
-} else if (rnd > 70 && rnd < 100) {
-  alert(
-    `${username}, you have a ${rnd}% chance of being a match, which makes this pretty good. This could be the real thing!`
-  );
-} else if (rnd === 100) {
-  alert(
-    `${username}, you have a ${rnd}% chance of being a match, which is insanely perfect. Let nothing get in your way!`
-  );
-} else {
-  alert(
-    `${username}, you have a ${rnd}% chance of being a match, which is crap. Go home and do something productive instead!`
-  );
+// if (rnd < 70 && rnd > 50) {
+//   alert(
+//     `${username}, you have a ${rnd}% chance of being a match, which is okay. Go for it, but don't expect it to last!`
+//   );
+// } else if (rnd > 70 && rnd < 100) {
+//   alert(
+//     `${username}, you have a ${rnd}% chance of being a match, which makes this pretty good. This could be the real thing!`
+//   );
+// } else if (rnd === 100) {
+//   alert(
+//     `${username}, you have a ${rnd}% chance of being a match, which is insanely perfect. Let nothing get in your way!`
+//   );
+// } else {
+//   alert(
+//     `${username}, you have a ${rnd}% chance of being a match, which is crap. Go home and do something productive instead!`
+//   );
+// }
+function bmiCalculator(weight, height) {
+  const bmi = (weight * 703) / (height * height);
+  if (bmi < 18.5) {
+    interpretation = `Your BMI is ${bmi}, so you are underweight.`;
+  } else if (bmi > 18.5 && bmi < 24.9) {
+    interpretation = `Your BMI is ${bmi}, so you have a normal weight.`;
+  } else {
+    interpretation = `Your BMI is ${bmi}. so you are overweight.`;
+  }
+
+  //console.log(interpretation);
+  percentage.innerHTML = interpretation;
 }
+bmiCalculator(160, 71);
