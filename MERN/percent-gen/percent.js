f36658c5636964c6618cba4bcc83556838703a1a
wintercricket <hodhodhod2003@gmail.com>
2022-02-28 22:19:06 -0600

    add more conditions for a greater variety of alert responses

--- MERN/percent-gen/percent.js
+++ MERN/percent-gen/percent.js
@@ -10,12 +10,20 @@ rnd = Math.floor(rnd) + 1;
 let username = prompt("What is your name?");
 prompt("What is the other's name?");
 
-if (rnd >= 51) {
+if (rnd < 70 && rnd > 50) {
   alert(
-    `${username}, you have a ${rnd}% chance of being a match, which is good. Go for it!`
+    `${username}, you have a ${rnd}% chance of being a match, which is okay. Go for it, but don't expect it to last!`
+  );
+} else if (rnd > 70 && rnd < 100) {
+  alert(
+    `${username}, you have a ${rnd}% chance of being a match, which makes this pretty good. This could be the real thing!`
+  );
+} else if (rnd === 100) {
+  alert(
+    `${username}, you have a ${rnd}% chance of being a match, which is insanely perfect. Let nothing get in your way!`
   );
 } else {
   alert(
-    `${username}, you have a ${rnd}% chance of being a match, which makes this unlikely. Seek elsewhere!`
+    `${username}, you have a ${rnd}% chance of being a match, which is crap. Go home and do something productive instead!`
   );
 }
