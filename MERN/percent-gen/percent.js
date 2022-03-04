f0b81e94059ca746a58f2ac49e39c1cc7f54f912
wintercricket <hodhodhod2003@gmail.com>
2022-03-04 00:38:24 -0600

    create a press less automated calling

--- MERN/percent-gen/percent.js
+++ MERN/percent-gen/percent.js
@@ -63,17 +63,38 @@
 //   alert(`Call security and please remove ${guestName} from the club!`);
 // }
 
+// let output = [];
+// function fizzbuzz() {
+//   for (let i = 1; i <= 100; i++) {
+//     if (i % 15 === 0) {
+//       output.push("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//       output.push("Fizz");
+//     } else if (i % 5 === 0) {
+//       output.push("Buzz");
+//     } else {
+//       output.push(i);
+//     }
+//   }
+//   console.log(output);
+// }
+// fizzbuzz();
+
 let output = [];
+let count = 1;
+
 function fizzbuzz() {
-  for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0) {
-      output.push("Fizz");
-    } else if (i % 5 === 0) {
-      output.push("Buzz");
-    } else {
-      output.push(i);
-    }
+  count++;
+  if (count % 15 === 0) {
+    output.push("FizzBuzz");
+  } else if (count % 3 === 0) {
+    output.push("Fizz");
+  } else if (count % 5 === 0) {
+    output.push("Buzz");
+  } else {
+    output.push(count);
   }
-  alert(output);
+  console.log(output);
 }
+
 fizzbuzz();
