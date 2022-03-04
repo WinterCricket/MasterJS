df960c46b71e75dcd13e6511fdba1ec5d42a0431
wintercricket <hodhodhod2003@gmail.com>
2022-03-04 00:05:01 -0600

    wrapped fizzbuzz into a function

--- MERN/percent-gen/percent.js
+++ MERN/percent-gen/percent.js
@@ -64,13 +64,16 @@
 // }
 
 let output = [];
-for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0) {
-    output.push("Fizz");
-  } else if (i % 5 === 0) {
-    output.push("Buzz");
-  } else {
-    output.push(i);
+function fizzbuzz() {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+      output.push("Fizz");
+    } else if (i % 5 === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(i);
+    }
   }
+  alert(output);
 }
-alert(output);
+fizzbuzz();
