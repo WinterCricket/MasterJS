d29518e58d5b01478ab0545e925362e9912a64fb
wintercricket <hodhodhod2003@gmail.com>
2022-03-12 20:43:07 -0600

    dot notation for adding attr or prop

--- MERN/DOM/script.js
+++ MERN/DOM/script.js
@@ -1,5 +1,6 @@
 "strict use";
 
 const word = document.lastElementChild.lastElementChild.firstElementChild;
-word.innerHTML = "Sassy!";
+word.innerHTML = "Sassy! Get my drift?";
 // console.log(word);
+word.style.color = "maroon";
