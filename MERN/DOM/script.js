cb6a5aadd02eef741007d5769cb551d8dcb71f59
wintercricket <hodhodhod2003@gmail.com>
2022-03-25 01:01:14 -0500

    make some style changes using js

--- MERN/DOM/script.js
+++ MERN/DOM/script.js
@@ -2,3 +2,7 @@
 
 document.querySelector("ul").lastElementChild.innerHTML =
   "Goethe, after Shakespeare, wrote the sentence.";
+const html = document.getElementsByClassName("btn")[0].style;
+html.color = "red";
+html.backgroundColor = "black";
+html.borderRadius = "20px";
