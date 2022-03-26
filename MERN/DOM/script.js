3ab3b1962ab8c313a6cc1c9616e85b0e3faeac0e
wintercricket <hodhodhod2003@gmail.com>
2022-03-25 23:16:20 -0500

    use queryselector to manip h1 element

--- MERN/DOM/script.js
+++ MERN/DOM/script.js
@@ -2,8 +2,11 @@
 
 document.querySelector("ul").lastElementChild.innerHTML =
   "Goethe, after Shakespeare, wrote the sentence.";
-const html = document.getElementsByClassName("btn")[0].style;
-html.color = "red";
-html.backgroundColor = "black";
-html.borderRadius = "20px";
-document.querySelector("li a").style.color = "red";
+const btn = document.getElementsByClassName("btn")[0].style;
+btn.color = "red";
+btn.backgroundColor = "black";
+btn.borderRadius = "20px";
+
+const h1 = document.querySelector("h1");
+h1.style.fontSize = "6rem";
+//h1.style.visibility = "hidden";
