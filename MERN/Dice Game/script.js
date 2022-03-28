cb45f58e6644e2f22299e5ee6bb6a34cda9f14e4
wintercricket <hodhodhod2003@gmail.com>
2022-03-28 00:10:27 -0500

    setting up image to files

--- MERN/Dice Game/script.js
+++ MERN/Dice Game/script.js
@@ -1,4 +1,6 @@
-let die1 = Math.round(Math.random() * 5) + 1;
-document.querySelector(".die1").innerHTML = die1;
-let die2 = Math.round(Math.random() * 5) + 1;
-document.querySelector(".die2").innerHTML = die2;
+let die1 = Math.floor(Math.random() * 5) + 1;
+img1 = `images/dice${die1}.png`;
+document.querySelector(".img1").innerHTML = die1;
+let die2 = Math.floor(Math.random() * 5) + 1;
+img2 = `images/dice${die2}.png`;
+document.querySelector(".img2").innerHTML = die2;
