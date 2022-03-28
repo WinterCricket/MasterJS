5c20e896b8c379b6568628ebf510469ae0bc237c
wintercricket <hodhodhod2003@gmail.com>
2022-03-28 18:35:04 -0500

    allow image selection of dice to show up on app

--- MERN/Dice Game/script.js
+++ MERN/Dice Game/script.js
@@ -1,7 +1,6 @@
-const die1 = document.querySelector(".die1");
-const die2 = document.querySelector(".die2");
+const die1 = document.querySelector(".img1");
+const die2 = document.querySelector(".img2");
 let roll1 = Math.floor(Math.random() * 5) + 1;
-img1 = `images/dice${roll1}.png`;
 let roll2 = Math.floor(Math.random() * 5) + 1;
-img2 = `images/dice${roll2}.png`;
-console.log(img1);
+die1.src = `images/dice${roll1}.png`;
+die2.src = `images/dice${roll2}.png`;
