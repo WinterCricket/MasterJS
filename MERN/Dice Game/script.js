0df3e4c537d1f233926f9c97266522bb0f0d1b1e
wintercricket <hodhodhod2003@gmail.com>
2022-03-27 23:48:23 -0500

    get rnd for both dice

--- MERN/Dice Game/script.js
+++ MERN/Dice Game/script.js
@@ -1,2 +1,4 @@
-let dice = Math.round(Math.random() * 6) + 1;
-console.log(dice);
+let die1 = Math.round(Math.random() * 5) + 1;
+document.querySelector(".die1").innerHTML = die1;
+let die2 = Math.round(Math.random() * 5) + 1;
+document.querySelector(".die2").innerHTML = die2;
