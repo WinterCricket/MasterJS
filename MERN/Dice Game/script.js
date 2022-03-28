504a834d0b7403bc7583ef527c0dd8bc9d71f99b
wintercricket <hodhodhod2003@gmail.com>
2022-03-28 18:54:57 -0500

    add conditional for winner and draw

--- MERN/Dice Game/script.js
+++ MERN/Dice Game/script.js
@@ -1,6 +1,15 @@
+const header = document.querySelector("h1");
 const die1 = document.querySelector(".img1");
 const die2 = document.querySelector(".img2");
 let roll1 = Math.floor(Math.random() * 5) + 1;
 let roll2 = Math.floor(Math.random() * 5) + 1;
 die1.src = `images/dice${roll1}.png`;
 die2.src = `images/dice${roll2}.png`;
+
+if (roll1 > roll2) {
+  header.innerHTML = "Player 1 is a Winner!";
+} else if (roll1 < roll2) {
+  header.innerHTML = "PLayer 2 is a Winner!";
+} else {
+  header.innerHTML = "Draw!";
+}
