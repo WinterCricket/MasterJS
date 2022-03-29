2221c7693e77f035b2ed011d851fc6d9d6ab563d
wintercricket <hodhodhod2003@gmail.com>
2022-03-28 21:52:06 -0500

    do all this without template literals

--- MERN/Dice Game/script.js
+++ MERN/Dice Game/script.js
@@ -3,9 +3,13 @@ const die1 = document.querySelector(".img1");
 const die2 = document.querySelector(".img2");
 let roll1 = Math.floor(Math.random() * 5) + 1;
 let roll2 = Math.floor(Math.random() * 5) + 1;
-die1.src = `images/dice${roll1}.png`;
-die2.src = `images/dice${roll2}.png`;
+// die1.src = `images/dice${roll1}.png`;
+// die2.src = `images/dice${roll2}.png`;
 
+let source1 = "images/dice" + roll1 + ".png";
+let source2 = "images/dice" + roll2 + ".png";
+die1.setAttribute("src", source1);
+die2.setAttribute("src", source2);
 if (roll1 > roll2) {
   header.innerHTML = "Player 1 is a Winner!";
 } else if (roll1 < roll2) {
