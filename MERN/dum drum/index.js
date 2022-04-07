904d962cc99f05a1897052a20987b7f9247a7e65
wintercricket <hodhodhod2003@gmail.com>
2022-04-06 23:56:10 -0500

    create own methods and function and call them

--- MERN/dum drum/index.js
+++ MERN/dum drum/index.js
@@ -6,3 +6,15 @@ for (i of btns) {
 function buttonClick() {
   alert("Button Clicked!");
 }
+
+function addad(put1, put1) {
+  return put1 + put2;
+}
+
+function multipad(put1, put2) {
+  return put1 * put2;
+}
+function operator(put1, put2, operator) {
+  return operator(put1, put2);
+}
+console.log(operator(3, 6, multipad));
