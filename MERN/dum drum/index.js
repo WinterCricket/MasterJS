9d3caeb19d3c6d87fc4d4178279b86ff5a381062
wintercricket <hodhodhod2003@gmail.com>
2022-04-07 22:31:21 -0500

    add functions subtract and divide

--- MERN/dum drum/index.js
+++ MERN/dum drum/index.js
@@ -10,13 +10,19 @@ function buttonClick() {
 function add(put1, put2) {
   return put1 + put2;
 }
+function subtract(put1, put2) {
+  return put1 - put2;
+}
 
-function multipad(put1, put2) {
+function multiply(put1, put2) {
   return put1 * put2;
+}
+function divide(put1, put2) {
+  return put1 / put2;
 }
 function calculator(put1, put2, operator) {
   return operator(put1, put2);
 }
 //console.log(operator(3, 6, multipad));
 
-document.querySelector("footer").innerHTML = calculator(3, 6, add);
+document.querySelector("footer").innerHTML = calculator(3, 6, subtract);
