1c78d229a975284e95a094ae61224d9ba62c13be
wintercricket <hodhodhod2003@gmail.com>
2022-04-16 21:58:47 -0500

    try basic class assignment for sound

--- MERN/dum drum/index.js
+++ MERN/dum drum/index.js
@@ -1,28 +1,10 @@
-let btns = document.querySelectorAll(".drum");
+//let btns = document.querySelectorAll(".drum");
+const w = document.querySelector(".w");
 
-for (i of btns) {
-  i.addEventListener("click", buttonClick);
-}
+// for (i of btns) {
+//   i.addEventListener("click", buttonClick);
+// }
 function buttonClick() {
-  alert("Button Clicked!");
+  var audio = new Audio("sounds/crash.mp3");
+  audio.play();
 }
-
-function add(put1, put2) {
-  return put1 + put2;
-}
-function subtract(put1, put2) {
-  return put1 - put2;
-}
-
-function multiply(put1, put2) {
-  return put1 * put2;
-}
-function divide(put1, put2) {
-  return put1 / put2;
-}
-function calculator(put1, put2, operator) {
-  return operator(put1, put2);
-}
-//console.log(operator(3, 6, multipad));
-
-document.querySelector("footer").innerHTML = calculator(3, 6, subtract);
