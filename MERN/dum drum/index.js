ec47318fcc47578b4c1a2304bce7bb3cb4d34266
wintercricket <hodhodhod2003@gmail.com>
2022-04-18 02:24:29 -0500

    buttons turn letter white when clicked

--- MERN/dum drum/index.js
+++ MERN/dum drum/index.js
@@ -2,7 +2,7 @@ const numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-    console.log(this);
+    this.style.color = "white";
   });
 }
     
