d700ee7c0662baa787226bbaf99be3639b7f08ce
wintercricket <hodhodhod2003@gmail.com>
2022-04-18 02:14:20 -0500

    use this to sense and display which button was pushed

--- MERN/dum drum/index.js
+++ MERN/dum drum/index.js
@@ -1,8 +1,9 @@
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+const numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i < numberOfDrumButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    var audio = new Audio("sounds/crash.mp3");
-    audio.play();
+for (let i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+    console.log(this);
   });
 }
+    
+
