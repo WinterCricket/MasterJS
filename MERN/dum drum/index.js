b5462ee5c728f5534030b2bf340947635f0d1ef9
wintercricket <hodhodhod2003@gmail.com>
2022-05-03 23:58:56 -0500

    try to link sound to drum type

--- MERN/dum drum/index.js
+++ MERN/dum drum/index.js
@@ -1,9 +1,11 @@
 const numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfDrumButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-    this.style.color = "white";
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    // this.style.color = "white";
+    console.log(this.audio);
   });
 }
-    
 
+// var audio = new Audio("sounds");
+//     audio.play();
