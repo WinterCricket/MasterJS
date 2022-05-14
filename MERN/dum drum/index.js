b2ab3813e94d36bbfde0ae19ebec13444e60ab5d
wintercricket <hodhodhod2003@gmail.com>
2022-05-14 10:56:29 -0500

    assign buttons to mp3 files

--- MERN/dum drum/index.js
+++ MERN/dum drum/index.js
@@ -2,15 +2,14 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    // this.style.color = "white";
     var buttonInnerHTML = this.innerHTML;
     switch (buttonInnerHTML) {
       case "w":
-        var crash = new Audio("sounds/crash.png");
+        var crash = new Audio("sounds/crash.mp3");
         crash.play();
         break;
       case "a":
-        var kick = new Audio("sounds/kick.png");
+        var kick = new Audio("sounds/kick-bass.mp3");
         kick.play();
         break;
 
