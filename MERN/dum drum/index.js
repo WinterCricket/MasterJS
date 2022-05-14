818ade7b1d7e60991b9de7307197573f3d7b360d
wintercricket <hodhodhod2003@gmail.com>
2022-05-14 10:51:28 -0500

    add switch statements

--- MERN/dum drum/index.js
+++ MERN/dum drum/index.js
@@ -1,17 +1,21 @@
-const numberOfDrumButtons = document.querySelectorAll(".drum").length;
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (let i = 0; i < numberOfDrumButtons; i++) {
+for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     // this.style.color = "white";
-    switch (key) {
-      case value:
+    var buttonInnerHTML = this.innerHTML;
+    switch (buttonInnerHTML) {
+      case "w":
+        var crash = new Audio("sounds/crash.png");
+        crash.play();
+        break;
+      case "a":
+        var kick = new Audio("sounds/kick.png");
+        kick.play();
         break;
 
       default:
-        break;
+        console.log(buttonInnerHtml);
     }
   });
 }
-
-var audio = new Audio("sounds");
-audio.play();
