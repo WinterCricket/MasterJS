799159119c402452a22e53692d538ad9aa157d4e
wintercricket <hodhodhod2003@gmail.com>
2022-05-14 00:34:04 -0500

    use constructor functions to manufacture housekeeper objects

--- MERN/dum drum/elementCount.js
+++ MERN/dum drum/elementCount.js
@@ -1,11 +1,39 @@
-var liElements = document.getElementsByTagName("li");
-var howManyLi = liElements.length;
-for (var i = 0; i < howManyLi; i++) {
-  if (liElements[i].innerHTML === "") {
-    liElements[i].innerHTML = "Coming Soon to a Venue Near You!";
-  }
+// var liElements = document.getElementsByTagName("li");
+// var howManyLi = liElements.length;
+// for (var i = 0; i < howManyLi; i++) {
+//   if (liElements[i].innerHTML === "") {
+//     liElements[i].innerHTML = "Coming Soon to a Venue Near You!";
+//   }
+// }
+
+// console.log(liElements);
+
+// console.log(howManyLi);
+
+function HouseKeeper(name, age, hasWorkPermit, languages, topSkill) {
+  this.name = name;
+  this.age = age;
+  this.hasWorkPermit = hasWorkPermit;
+  this.languages = languages;
+  this.topSkill = topSkill;
 }
 
-console.log(liElements);
+let houseKeeper1 = new HouseKeeper(
+  "Ria Dyanos",
+  34,
+  true,
+  ["French", "Dutch", "Portuguese", "English"],
+  "fast clean"
+);
 
-console.log(howManyLi);
+let houseKeeper2 = new HouseKeeper(
+  "Olga Olgala",
+  101,
+  false,
+  [" French", " English", " Pigeon", " ASL"],
+  "corny jokes"
+);
+
+alert(
+  `I'm ${houseKeeper2.name}, your ${houseKeeper2.topSkill} housekeeper. I speak ${houseKeeper2.languages}.`
+);
